7da1fa4112808e4c0efb6584f638bea9c45e20a9
James Derry <jderry@derryitconsulting.com>
2014-03-11 18:09:37 -0500

    Initial commit.

--- iij-session2-c/ViewController.m
+++ iij-session2-c/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSMutableArray *articles;  //to be an array of dictionaries
+}
 @end
 
 @implementation ViewController
@@ -18,6 +20,47 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    //a free api from Pearson for Financial Times articles
+    
+    // ten articles downloaded by default
+    NSString *articleUrl = @"https://api.pearson.com/v2/ft/articles";
+    NSURL *url = [NSURL URLWithString:articleUrl];
+    NSData *articleData = [NSData dataWithContentsOfURL:url];
+    NSError *error;
+    
+    NSDictionary *parsedJSON = [NSJSONSerialization JSONObjectWithData:articleData options:NSJSONReadingMutableContainers error:&error];
+    
+    articles = [NSMutableArray arrayWithArray:[parsedJSON objectForKey:@"results"]];
+    
+    NSLog(@"%@", articles);
+}
+
+#pragma mark - UITableView Delegate Methods
+
+
+#pragma mark - UITableView Datasource Methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [articles count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *identifier = @"articleCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    
+    NSDictionary *articleInfo = [articles objectAtIndex:indexPath.row];
+    NSString *title = [articleInfo objectForKey:@"headline"];
+    cell.textLabel.text = title;
+    
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning
