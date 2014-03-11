a45e2c91c00ec8bfc59d856060b5ce015c4f3465
James Derry <jderry@derryitconsulting.com>
2014-03-11 17:11:10 -0500

    Initial Commit

---
+++ iij-session2-c/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iij-session2-c
+//
+//  Created by James Derry on 3/11/14.
+//  Copyright (c) 2014 James Derry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iij-session2-c/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iij-session2-c
+//
+//  Created by James Derry on 3/11/14.
+//  Copyright (c) 2014 James Derry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
